32eb7374c386a79bc489ddc60bd298c39a1b94aa
Carlton Aikins <76491344+31Carlton7@users.noreply.github.com>
2021-04-22 10:34:30 -0400

    Create cyoa-layout.java

---
+++ unit-5/5.3/cyoa-layout.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CYOA
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        
+        // Intro
+        // Create variables and Scanner
+        
+        // User choice
+        
+        // Display description based off of choice
+        
+        // Repeat or break
+    }
+}
