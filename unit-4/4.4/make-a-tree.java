e90f97ee469f2a13274d177edca65304d5e92efa
Carlton Aikins <carltonaikins7@gmail.com>
2021-11-04 19:00:58 -0400

    Update make-a-tree.java

--- unit-4/4.4/make-a-tree.java
+++ unit-4/4.4/make-a-tree.java
@@ -2,22 +2,26 @@ public class TreeOfStars
 {
     public static void main(String[] args)
     {
-       // Call makeATree 
-       makeATree();
+         // Call makeATree 
+	     makeATree();
     }
     public static void makeATree()
     {
-        // Your code goes here!!!
-        
-            System.out.println("          * ");
-            System.out.println("         * * ");
-            System.out.println("        * * * ");
-            System.out.println("       * * * * ");
-            System.out.println("      * * * * * ");
-            System.out.println("     * * * * * * ");
-            System.out.println("    * * * * * * * ");
-            System.out.println("   * * * * * * * * ");
-            System.out.println("  * * * * * * * * * ");
-
-    }
+       int numLevels = 9;
+       int longestLevelWidth = (numLevels * 2) + 1;
+       for(int i = 0; i < numLevels; i++) {
+           int spacing = longestLevelWidth/2 - i;
+           
+           for(int j = 0; j < spacing; j++) {
+               System.out.print(" ");
+           }
+           
+           for(int k = 0; k <= i; k++) {
+               System.out.print(" *");
+           }
+           
+           System.out.print(" ");
+           System.out.println();
+        }
+     }
 }
